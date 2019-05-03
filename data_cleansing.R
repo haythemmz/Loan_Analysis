a78ce6c7d72b6e0e8d7127cdecd1c9d62ce16500
haithem mzoughi <hmzoughi@afrd.com>
2019-05-03 16:01:03 +0100

    yes

--- data_cleansing.R
+++ data_cleansing.R
@@ -23,5 +23,10 @@ drop_columns <- function (mis,df,threshold){
 }
 new <- drop_columns(mis = missing, df= loan , threshold = 90)
 new_missi <- calcule_missing_values(new)
-head(new_missi)
+drop_useless <- function (df,delete){
+  newData <- df[,!(names(df) %in% delete)] 
+  return (newData)
+}
+
+head(calcule_missing_values(drop_useless(new,c("mths_since_last_delinq"))))
 
